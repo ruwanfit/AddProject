cdf74345658d234b5199c533a30cccf66df46af1
Ruwan Ananda <ruwans@simct.com>
2020-10-28 13:52:02 +0530

    temp comment
    Signed-off-by: Ruwan Ananda <ruwans@simct.com>

--- GitHubTest1/GitHubTest1/Source.cpp
+++ GitHubTest1/GitHubTest1/Source.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include "UnitTest.h"
+//#include "UnitTest.h"
 #include <iostream>
 
 void main()
 {
 	int i = 1;
 	std::cout << "Hello jenkins...";
-	pldl::UnitTest unitTest;
+	//pldl::UnitTest unitTest;
 }
