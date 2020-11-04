d77d93994bfd03c9428cdb6e78fd781211713ea3
Ruwan Ananda <ruwans@simct.com>
2020-11-04 15:41:10 +0530

    google api code added to source
    Signed-off-by: Ruwan Ananda <ruwans@simct.com>

--- GitHubTest1/GitHubTest1/Source.cpp
+++ GitHubTest1/GitHubTest1/Source.cpp
@@ -1,11 +1,12 @@
 //#include<stdio.h>
-//#include "UnitTest.h"
+#include "UnitTest.h"
 #include <iostream>
 
 int main()
 {
 	//int i = 1;
 	std::cout << "Hello jenkins...";
+	pldl::UnitTest utest = pldl::UnitTest();
 	return 0;
 	//pldl::UnitTest unitTest;
 }
